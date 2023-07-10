6785bebcb6f8c7bfd99d19cccb00b8e86c8716fc
Shorouk <shorouk.mohamed93@gmail.com>
2023-07-10 19:13:51 +0300

    add camera delegate Handler

--- Sources/Valify/Camera/CameraNavigationViewController.swift
+++ Sources/Valify/Camera/CameraNavigationViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 public class CameraNavigationViewController: UINavigationController {
     public var onCompleted: ((UIImage?, Error?) -> Void)?
+    public var cameraDelegate: CameraHandler?
 
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,9 +21,19 @@ public class CameraNavigationViewController: UINavigationController {
             return nil
         }
 
-        onCompleted?(cameraViewController.capturedImage, cameraViewController.capturedError)
+        cameraViewController.onCompleted = { [weak self] image, error in
+            if let error = error {
+                   self?.cameraDelegate?.didFinishWithErrors(error: error)
+               } else if let image = image {
+                   self?.cameraDelegate?.didFinishSucceded(image: image)
+               }
+        }
+//        onCompleted?(cameraViewController.capturedImage, cameraViewController.capturedError)
 
         return super.popViewController(animated: animated)
     }
 }
-
+public protocol CameraHandler {
+    func didFinishSucceded(image: UIImage)
+    func didFinishWithErrors(error: Error)
+}
